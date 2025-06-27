4c870e70bc0d4de4aaa00a0cd2f933756e4371de
Lucas Paiva <lpaiva234@gmail.com>
2025-06-27 16:57:56 -0300

    add event listeners for buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import "./style.css";
 import { loadHomepage } from "./home";
 import { loadMenu } from "./menu";
+import { loadReservations } from "./reservations";
 
-// loadHomepage();
-loadMenu();
+const contentDiv = document.querySelector("#content");
+const homeButton = document.querySelector(".home-button");
+const menuButton = document.querySelector(".menu-button");
+const reservationButton = document.querySelector(".reservation-button");
+
+function clearDivContents(div) {
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+loadHomepage();
+
+homeButton.addEventListener("click",() => {
+    contentDiv.classList.remove("menu");
+    clearDivContents(contentDiv);
+    loadHomepage();
+});
+
+menuButton.addEventListener("click",() => {
+    clearDivContents(contentDiv);
+    loadMenu();
+});
+
+reservationButton.addEventListener("click",() => {
+    contentDiv.classList.remove("menu");
+    clearDivContents(contentDiv);
+    loadReservations();
+});
